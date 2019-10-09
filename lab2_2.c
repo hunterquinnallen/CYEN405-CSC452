9d14619021f8cad73cbdd8841ab9608c11c49a83
Hunter Allen <hunterquinnallen@hotmail.com>
2019-10-08 20:58:22 -0500

    Update lab2_2.c

--- lab2_2.c
+++ lab2_2.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
 	start = clock();
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_rank(MPI_COMM_WORLD, &size);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	printf("Hello hqa001 or Hunter A from #%d of %d!\n", rank, size);
 	end = clock();
 	double t = (double) (end-start)/CLOCKS_PER_SEC;
